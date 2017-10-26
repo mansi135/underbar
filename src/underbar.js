1170c14930f1a5971de847a2385482cd2d6eba33
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2017-10-26 15:33:26 -0700

    completed some methods not related to closure in Underbar2/2

--- src/underbar.js
+++ src/underbar.js
@@ -255,12 +255,26 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(result,item) {
+      if(iterator)
+        return result && Boolean(iterator(item));
+      else
+        return result && Boolean(item);
+    },true);
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    return _.reduce(collection, function(result,item) {
+      if(iterator)
+        return result || Boolean(iterator(item));
+      else
+        return result || Boolean(item);
+    },false);
+
   };
 
 
@@ -283,11 +297,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var k in arguments[i]) {
+        obj[k] = arguments[i][k] ;
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var k in arguments[i]) {
+        if (!(k in obj)){
+          obj[k] = arguments[i][k] ;
+        }
+      }
+    }
+    return obj;
   };
 
 
